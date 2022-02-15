954635bbef1aa4ba1abb1039ebdf7bc9e0caf82a
tyronsur <emilio@tyronsur.es>
2022-02-15 23:56:30 +0100

    [RUTAS] PROGRESO DEL 10%

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,4 @@
 <script type="text/javascript" src="{{ asset('scripts/bootstrap.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('scripts/custom.js') }}"></script>
+@yield('custom-js-page')
 </body>
--- resources/views/rutas/index.blade.php
+++ resources/views/rutas/index.blade.php
@@ -1,10 +1,47 @@
 @extends('layouts.plantilla')
 
-@section('title', 'Home')
+@section('title', 'MI RUTA')
+
+@section('custom-js-page')
+<script type="text/javascript" src="{{ asset('js/countdown.js') }}"></script>
+@endsection
 
 @section('content')
+<div class="card card-style">
+    <div class="content text-center">
+    <i class="fa fa-clock fa-7x color-highlight mb-5"></i>
+    <h1 class="font-36 font-800 mb-4">Proxima Ruta!</h1>
+    <p class="text-center">RUTA 1 [9:15 A 11:00]</p>
+    <h3 class="text-center">
+        TORREDELCAMPO<br>TORREDONJIMENO<br>MARTOS<br>PORCUNA<br>ARJONA<br>ANDUJAR
+        </h3>
+    <div class="countdown row mb-0 mt-5" style="background-color: rgb(238, 238, 238);">
+    
+    <div class="disabled">
+    <h1 class="mb-0 color-theme font-30" id="years"></h1>
+    <p class="mt-n1 color-theme font-11 opacity-30">years</p>
+    </div>
+    <div class="disabled">
+    <h1 class="mb-0 color-theme font-30" id="days"></h1>
+    <p class="mt-n1 color-theme font-11 opacity-30">days</p>
+    </div>
+    <div class="col-4">
+    <h1 class="mb-0 color-theme font-30" id="hours"></h1>
+    <p class="mt-n1 color-theme font-11 opacity-30">Horas</p>
+    </div>
+    <div class="col-4">
+    <h1 class="mb-0 color-theme font-30" id="minutes"></h1>
+    <p class="mt-n1 color-theme font-11 opacity-30">Minutos</p>
+    </div>
+    <div class="col-4">
+    <h1 class="mb-0 color-theme font-30" id="seconds"></h1>
+    <p class="mt-n1 color-theme font-11 opacity-30">Segundos</p>
+    </div>
+    </div>
+    </div>
+    </div>
 
-<div class="menu-title"><h1>MARTES</h1><p class="color-highlight">TODAS LAS RUTAS DE HOY</p><a href="#" class="close-menu"><i class="fa fa-times"></i></a></div>
+<!--<div class="menu-title"><h1>MARTES</h1><p class="color-highlight">TODAS LAS RUTAS DE HOY</p><a href="#" class="close-menu"><i class="fa fa-times"></i></a></div>
 <div class="card card-style">
 <div class="content">
 <div class="menu-title"><h2>MAÑANA</h2></div>
@@ -27,7 +64,7 @@
     <div data-day="Thursday" class="working-hours"><p>RUTA 3</p> <p>16:15</p> <p>18:45</p></div>
     
     </div>
-    </div>
+    </div>-->
 
 <!-- Layout Required All Views -->
 </div></div>
